e71f0d145009efffa81a82b028863825565ff06d
Ivan Vasic <ivasic@gmail.com>
2015-06-19 15:40:17 +0200

    Decode UInt

--- Fargo/Types/StandardTypes.swift
+++ Fargo/Types/StandardTypes.swift
@@ -1,48 +1,57 @@
 import Foundation
 
 extension String: Decodable {
-  public static func decode(j: JSON) -> Decoded<String> {
-    switch j {
-    case let .String(s): return pure(s)
-    default: return j.typeMismatch("String")
-    }
-  }
+	public static func decode(j: JSON) -> Decoded<String> {
+		switch j {
+		case let .String(s): return pure(s)
+		default: return j.typeMismatch("String")
+		}
+	}
 }
 
 extension Int: Decodable {
-  public static func decode(j: JSON) -> Decoded<Int> {
-    switch j {
-    case let .Number(n): return pure(n as Int)
-    default: return j.typeMismatch("Int")
-    }
-  }
+	public static func decode(j: JSON) -> Decoded<Int> {
+		switch j {
+		case let .Number(n): return pure(n as Int)
+		default: return j.typeMismatch("Int")
+		}
+	}
+}
+
+extension UInt: Decodable {
+	public static func decode(j: JSON) -> Decoded<UInt> {
+		switch j {
+		case let .Number(n): return pure(n as UInt)
+		default: return j.typeMismatch("UInt")
+		}
+	}
 }
 
 extension Int64: Decodable {
-  public static func decode(j: JSON) -> Decoded<Int64> {
-    switch j {
-    case let .Number(n): return pure(n.longLongValue)
-    default: return j.typeMismatch("Int64")
-    }
-  }
+	public static func decode(j: JSON) -> Decoded<Int64> {
+		switch j {
+		case let .Number(n): return pure(n.longLongValue)
+		default: return j.typeMismatch("Int64")
+		}
+	}
 }
 
 extension Double: Decodable {
-  public static func decode(j: JSON) -> Decoded<Double> {
-    switch j {
-    case let .Number(n): return pure(n as Double)
-    default: return j.typeMismatch("Double")
-    }
-  }
+	public static func decode(j: JSON) -> Decoded<Double> {
+		switch j {
+		case let .Number(n): return pure(n as Double)
+		default: return j.typeMismatch("Double")
+		}
+	}
 }
 
 extension Bool: Decodable {
-  public static func decode(j: JSON) -> Decoded<Bool> {
-    switch j {
-    case let .Number(n): return pure(n as Bool)
-    default: return j.typeMismatch("Bool")
-    }
-  }
+	public static func decode(j: JSON) -> Decoded<Bool> {
+		switch j {
+		case let .Number(n): return pure(n as Bool)
+		default: return j.typeMismatch("Bool")
+		}
+	}
 }
 
 extension Float: Decodable {
